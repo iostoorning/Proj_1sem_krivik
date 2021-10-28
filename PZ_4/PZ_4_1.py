6ffb6f2c79021f3c5f0b01cdae95e304f61e20a6
iostoorning <iostoorning@gmail.com>
2021-10-28 11:58:22 +0300

    1st done

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -7,9 +7,18 @@ while True:
             continue
         else:
             t = B - A
-            while t != 0:
-
+            n = 1
+            i = 0
+            while t != -1:
+                i += 1
+                while n != 0:
+                    print(A)
+                    n -= 1
+                A += 1
                 t -= 1
+                n = + 1 + i
         break
     except ValueError:
         print("Ошибка значения! Вводите целые числа")
+    except TypeError:
+        print('Ошибка ввода! Введитте целые числа')
