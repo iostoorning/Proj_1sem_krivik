2cb2acb7ac672132f764659ea3872d05e5130e42
iostoorning <iostoorning@gmail.com>
2021-10-28 12:12:57 +0300

    2nd done

---
+++ PZ_4/PZ_4_2.py
@@ -0,0 +1,19 @@
+
+while True:
+    try:
+        N = int(input("Введите первое число "))
+        K = int(input("Введите второе число "))
+        result = 0
+        if N <= K:
+            print("Резуьтат равен ", result)
+            print("Остаток равен", K)
+        else:
+            while not N < K:
+                N -= K
+                result += 1
+            print("Резуьтат равен ", result)
+            print("Остаток равен ", N)
+    except ValueError:
+        print("Ошибка! Введите целые положительные числа")
+    except TypeError:
+        print("Ошибка! Введите целые положительные числа")
