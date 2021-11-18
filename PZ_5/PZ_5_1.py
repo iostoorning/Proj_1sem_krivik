80dd38ea51ec703172113dc240bc7ec9e4a7b8e3
iostoorning <iostoorning@gmail.com>
2021-11-18 14:43:34 +0300

    .

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        a = 0
+        def charnumber(x):
+            x = len(input("Введите символы "))
+            print(x)
+        charnumber(a)
+        break
+    except ValueError:
+        print("Ошибка значения! Введите символы")
