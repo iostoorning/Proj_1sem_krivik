24d2d450d1e60e614b48c5d4ca27242ffd49bf0c
iostoorning <iostoorning@gmail.com>
2021-12-23 00:35:22 +0300

    .

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -1,33 +1,23 @@
-while True:
-    try:
-
-        def RectPS(x, xx, y, yy, P, S):
-            if 0 < xx > x < 0:
-                P = 2 * (xx + x) + 2 * (yy + y)
-                S = (xx + x) * (yy + y)
-            elif 0 < x > xx < 0:
-                P = 2 * (x + xx) + 2 * (y + yy)
-                S = (x + xx) * (y + yy)
-            elif xx > x:
-                P = 2 * (xx - x) + 2 * (yy - y)
-                S = (xx - x) * (yy - y)
-            elif x > xx:
-                P = 2 * (x - xx) + 2 * (y - yy)
-                S = (x - xx) * (y - yy)
-            elif P < 0:
-                P *= -1
-            elif S < 0:
-                S *= -1
-            S = round(S)
-            P = round(P)
-            print("Площадь равна = ", S, "Периметр равен = ", P)
+# Описать функцию RectPS(x1,y1,х2,y2,P, S), вычисляющую периметр P и площадь S
+# прямоугольника со сторонами, параллельными осям координат, по координатам (х1, y1),
+# (х2, y2) его противоположных вершин (x1, y1, x2, y2 — входные, P и S — выходные
+# параметры вещественного типа). С помощью этой функции найти периметры и площади
+# трех прямоугольников с данными противоположными вершинами.
+import random
 
 
-        x1 = float(input("Введите первую координату первой стороны "))
-        x2 = float(input("Введите вторую координату первой стороны "))
-        y1 = float(input("Введите первую координату второй стороны "))
-        y2 = float(input("Введите вторую координату второй стороны "))
-        RectPS(x1, x2, y1, y2, P=0, S=0)
-        break
-    except ValueError:
-        print("Ошибка! Вводите числа")
+def RectPS(x, xx, y, yy):
+    a = abs(x - xx)
+    b = abs(y - yy)
+    print("a = ", a)
+    print("b = ", b)
+    print("P =", 2 * (a + b))
+    print("S = ", a * b)
+    return
+
+
+x1, x2 = random.sample(range(-10, 10), 2)
+y1, y2 = random.sample(range(-10, 10), 2)
+print("x1 = ", x1, "; y1 = ", y1)
+print("x2 = ", x1, "; y2 = ", y2)
+RectPS(x1, y1, x2, y2, )
