bc9b8843c6cf3ee3731eb59b9ba9470600e45995
iostoorning <iostoorning@gmail.com>
2021-12-23 01:27:38 +0300

    .

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,8 +1,19 @@
-
+# Дан список ненулевых целых чисел размера N. Проверить, образуют ли его элементы
+# геометрическую прогрессию. Если образуют, то вывести знаменатель прогрессии, если
+# нет — вывести 0.
 while True:
     try:
-
-        numbers = list(input("Введите последовательность чисел больше 0"))
-        list(exception)
-        if numbers[0:] <=  :
-            print
+        x = input("Введите список чисел через пробел: ")
+        x.split()
+        NList = list(x)
+        y = float(NList[2]) / float(NList[0])
+        for i in range(len(NList) - 1):
+            if y == float(NList[i + 2]) / float(NList[2]):
+                continue
+            else:
+                q = 0
+                break
+        print(y)
+        break
+    except TypeError:
+        print("Ошибка значения! Вводите числа")
