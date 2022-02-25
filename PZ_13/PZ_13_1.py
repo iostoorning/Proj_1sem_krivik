9807a6a6d4cc9c996abfc22ab897d08f3be4fecd
iostoorning <iostoorning@gmail.com>
2022-02-25 18:01:37 +0300

    .

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,8 +1,8 @@
+# Даны значения роста 20 юношей. Определить сколько юношей будут направленны в баскетбольную команду (рост от 190)
+# и сколько в футбольную (остальные)
 from random import randint
 people = []
 for i in range(20):
     people.append(randint(160, 210))
-basketball = [n*1 for n in people if n >= 190]
-football = []
-print("Кол-во футболистов: ", len(set(people) - set(basketball)))
-print("Кол-во баскетболистов: ", len(basketball))
+print("Кол-во футболистов: ", len(list(filter(lambda x: x < 190, people))))
+print("Кол-во баскетболистов: ", len(list(filter(lambda x: x >= 190, people))))
