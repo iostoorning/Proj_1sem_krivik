c8b64882d19dba39b5e8636b63b1a41684bd6aff
iostoorning <iostoorning@gmail.com>
2022-05-20 20:38:13 +0300

    .

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,4 +1,8 @@
 # В исходном текстовом файле (Dostoevsky.txt) найти все годы деятельности писателя
 # (например, 1821 года, 1837 год, 1843 году и так далее по всему тексту). Посчитать
 # Посчитать кол-во элементов
-open(Dostoevsky.txt)
+import re
+buf = open("Dostoevsky.txt", "r")
+text = buf.read()
+result = re.findall(r'год', text)
+print(len(result))
