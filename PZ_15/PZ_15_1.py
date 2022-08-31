035a27d0fa1556c022733a854373fb00529087f5
iostoorning <iostoorning@gmail.com>
2022-08-31 19:22:26 +0300

    .

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -3,12 +3,14 @@
 import random as rnd
 import numpy as np
 
+n = 0
+i = 0
 arr1 = np.matrix([[rnd.randrange(-2, 5), rnd.randrange(-2, 5)],
-                  [rnd.randrange(-2, 5), rnd.randrange(-2, 5)]])
+                 [rnd.randrange(-2, 5), rnd.randrange(-2, 5)]])
 result = []
 for n in range(2):
     for i in range(2):
-        if arr1[[n][i]] < 0:
-            result = [result.append(arr1[[n][i]])]
+        if arr1[[n], [i]] < 0:
+            result.append(arr1[[n], [i]])
 
-print(result)
+print(len(result))
